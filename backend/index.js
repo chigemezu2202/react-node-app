4364b8566eaaf769c2f38c8b098b65444e8f6da9
chigemezu2202 <chigemezuezimoha@gmail.comgit config --global user.name chigemezu2202git config --global user.email chigemezuezimoha@gmail.com>
2024-09-10 12:49:36 +1300

    Production Ready Redeploy

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 //TODO: Enable CORS middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true}))
+app.use(cors({ origin:  process.env.CLIENT_URL, credentials: true}))
 
 //TODO: Middleware to parse JSON request bodies  **
 //Hack: allows us to parse incomming & ougoing request: req.body in json format
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { create } from "zustand";
 
 // Hack: Auth URL Variable
-const API_URL = "http://localhost:5000/api/auth";
+
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 
 // Hack: Credentials
 // Hack: At every request axios put cookies at request header, making cookie to be included
